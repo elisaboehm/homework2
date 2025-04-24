360f9ae4e1bb652097847936af70e734a3e723c0
Elisa Boehm <elisaboehm10@gmail.com>
2025-04-24 08:04:28 -0400

    Added an event in js and changed the cursor style in css

--- js/homework2.js
+++ js/homework2.js
@@ -1,1 +1,5 @@
-alert('abc');
+document.getElementById('first-paragraph').addEventListener('click', event => {
+    var style = event.target.style;
+    style.background = 'blue';
+    style.color = 'red';
+ })
